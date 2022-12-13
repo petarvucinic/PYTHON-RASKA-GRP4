5f7aefd42e531dd93baf0f91af6e4d3f351d5ddd
petarvucinic <87143624+petarvucinic@users.noreply.github.com>
2022-12-13 14:33:43 +0100

    class 8/12

---
+++ kurs.py
@@ -0,0 +1,285 @@
+
+# broj =  5
+# rec = "iudcdiu323233"
+# rec2 = "1234567890"
+# broj = 1234567890
+
+
+# -----------------------------
+
+# broj = int(input("unesite neki broj: "))
+# broj2= int(input("unesite neki broj: "))
+
+# print(broj + broj2)
+
+# -------------------------
+
+
+# print(True and True)
+# print(True and False)
+# print(False and True)
+# print(False and False)
+
+# print(50 * "-")
+
+# print(True or True)
+# print(False or False)
+# print(True or False)
+# print(False or True)
+
+# print(50 * "-")
+
+# print(not True)
+# print(not False)
+
+
+# +-------
+
+
+# print(5>2 and 6==4 or 5!=4)
+#     true and false or true
+#          false or true
+#              true
+
+
+# print(1 > 1 or 3 < 4 or 4 > 5 and 2 > 1 and 9 > 8 or 7 < 6)
+#     false or true
+#             true  or  false and true and true or false
+#                 true and true and true or false
+#                      true and true or false
+#                             true or false
+
+# print(not 2 > 1 and 3 < 4 or 4 > 5 and 2 > 1 and 9 > 8 or 7 < 6)
+#       false and true 
+#            false or false
+#                 false      and true
+#                              false and true
+#                                     false or false
+# false
+
+
+# -----------------------------
+
+
+# broj = 5
+
+# broj2 = float(broj)
+# broj3 = str(broj)
+
+# print(broj2)
+# print(broj3)
+# print('')
+
+
+# broj = 5.99
+# print(int(broj))
+
+
+# rec = "fegyiwfecf"
+# rec = "1234567890"
+
+# print(float(rec))
+
+# print(bool(6))
+# print(bool(0))
+
+# print(bool("hydwge"))
+# print(bool(""))
+
+
+# 
+
+
+# x = int(input("unesite neki broj: "))
+# y = int(input("unesite neki broj: "))
+
+# vrIzraza = 8**0.5 - (x*y*(3**0.5))**7
+
+# print(vrIzraza)
+
+
+# -----------------
+
+
+# rec = "gimnazija"
+# #      012345678
+# #             -2 -1
+# print(rec[1:6:2])
+# print(rec[ : :2])
+# print(rec[ : : -1])
+
+# print(rec[2])
+# print(rec[7])
+# print(rec[-2])
+
+# txt = " ovo je kurs pajtona "
+
+# print(txt.upper())
+# print(len(txt))
+# # txt = txt.strip()
+
+
+
+# print(txt)
+
+
+# strrrr = "1234567890"
+
+# print(strrrr.isdigit())
+
+# rec = "kurs"
+
+# rec = rec.replace("k", "K")
+
+# print(rec)
+
+
+# ---------------------------
+
+# rec = str(input("unesite neku rec: "))
+
+# print(rec == rec[::-1])
+
+# --
+
+
+# rec = "adaid"
+
+# rec = rec.replace("a", "A")
+
+# ---
+
+# rec = input("unesite neku vrednost: ")
+
+# rec = rec.replace(rec[0], rec[0].upper())
+
+# print(rec)
+
+
+# ----------------------
+
+# M = int(input("unesite br dana: "))
+# N = int(input("unesite br dana: "))
+
+# zajedno = (1/M + 1/N)**(-1)
+
+# print(zajedno)
+
+# ----------------------------
+
+
+# if False:
+#     print("if blok")
+# else:
+#     print("else blok")
+
+
+# broj1 = 4
+# broj2 = 4
+
+# if broj1 > broj2:
+#     print("broj1 je veci od broja2")
+# elif broj1 == broj2:
+#     print("broj1 je jednak be2")
+# else:
+#     print("broj2 je veci od broja1")
+
+
+# ----------------------------------
+
+
+# a = int(input("unesite duzinu stranice a: "))
+# b = int(input("unesite duzinu stranice b: "))
+
+# if a == b:
+#     print(f"Povrsina kvadrata je {a**2}")
+# else:
+#     print(f"Povrsina pravouganoika je {a*b}")
+
+
+# ----
+
+# rec = input("unesite neku rec: ")
+
+# python == cdshycgy
+# False
+# ana == ana
+# if rec == rec[::-1]:
+#     print("rec je palindrom")
+# else:
+#     print("rec nije palnidrom")
+
+# -------------------------
+
+# 1 - fizzbuzz
+# 2 - psece godine
+
+# a = int(input("unesite duzinu stranice: "))
+# b = int(input("unesite duzinu stranice: "))
+# c = int(input("unesite duzinu stranice: "))
+
+# if a+b>c and a+c>b and b+c>a:
+#     if a == b != c or a == c != b or b == c != a:
+#         print("jednakokeaki")
+#     elif a != b != c:
+#         print("raznostrani")
+#     else:
+#         print("jednakostranicni")
+#         # print("jednakokraki")
+# else:
+#     print("trougao ne postoji")
+ 
+
+# -----------------
+
+# broj = int(input("unesite neki broj: "))
+
+# if broj % 5 == 0 and broj % 3 == 0:
+#     print("fizzbuzz")
+# elif broj % 5 == 0:
+#     print("buzz")
+# elif broj % 3 == 0:
+#     print("fizz")
+
+# psece_godine = int(input("Uneiste sbcuiu: "))
+
+# if psece_godine < 3:
+#     print(psece_godine * 7)
+# else:
+#     print(2*7 + (psece_godine - 2)*5)
+
+
+# -----------------------------
+
+# br1 = int(input("Unesite  neki br: "))
+# br2 = int(input("Unesite  neki br: "))
+# op = input("unesite op +-*/: ")
+
+# if "+" in op:
+#     print(br1+br2)
+# elif "-" in op:
+#     print(br1-br2)
+# elif "*" in op:
+#     print(br1*br2)
+# elif "/" in op:
+#     print(br1/br2)
+
+# --------------------------
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
